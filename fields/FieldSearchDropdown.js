2df020bb2b24c087477de7097bc6174948c65c3a
DinoscapeProgramming <emailfromdinoscape@gmail.com>
2026-03-15 22:58:11 +0100

    feat: improve search

--- fields/FieldSearchDropdown.js
+++ fields/FieldSearchDropdown.js
@@ -11,6 +11,8 @@ module.exports = (Blockly) => (
 
       const search = document.createElement("input");
 
+      search.spellcheck = false;
+
       Object.assign(search.style, {
         color: "#575E75",
         width: "calc(100% - 10px)",
@@ -26,15 +28,44 @@ module.exports = (Blockly) => (
 
       menu.prepend(search);
 
+      requestAnimationFrame(() => {
+        menu.querySelector(".blocklyMenuItemSelected").scrollIntoView({ block: "nearest" });
+
+        requestAnimationFrame(() => {
+          search.focus();
+        });
+      });
+
       const items = Array.from(menu.querySelectorAll(".blocklyMenuItem"));
+      const separators = Array.from(menu.querySelectorAll(".blocklyMenuSeparator"));
 
       search.addEventListener("input", () => {
+        menu.scrollTop = 0;
+
         const value = search.value.toLowerCase();
 
         items.forEach((item) => {
           const text = item.textContent.toLowerCase();
 
           item.style.display = text.includes(value) ? "" : "none";
+        });
+
+        separators.forEach((separator) => {
+          let previousElement = separator.previousElementSibling;
+          let nextElement = separator.nextElementSibling;
+
+          while (previousElement && (previousElement.style.display === "none")) {
+            previousElement = previousElement.previousElementSibling;
+          };
+
+          while (nextElement && (nextElement.style.display === "none")) {
+            nextElement = nextElement.nextElementSibling;
+          };
+
+          separator.style.display = (
+            previousElement?.classList.contains("blocklyMenuItem") &&
+            nextElement?.classList.contains("blocklyMenuItem")
+          ) ? "" : "none";
         });
       });
 
